16ed05f66168eaf26ab12c352dc9fd3d44610fbc
Megan Swenson <mrsmeganswenson@gmail.com>
2020-07-10 18:54:04 -0600

    Got cards to randomize on click.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ class App extends React.Component {
     highScore: 0
   };
 
+  componentWillMount() {
+    // restartGame();
+    this.randomizeCards();
+
+  }
+
   increaseScore = () => {
     this.setState({ currentCount: this.state.currentCount + 1 })
   }
@@ -20,12 +26,54 @@ class App extends React.Component {
 
   // Function to change highScore state to highest score
   getHighScore = () => {
+    let newHighScore = this.state.currentCount;
+    if (this.state.currentCount > this.state.highScore) {
+      this.setState({ highScore: newHighScore })
+      //this.restartGame();
+    }
+  }
+
+  // Function to randomize rendered characters
+  randomizeCards = () => {
+    let people = this.state.characters;
+    let i = people.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = people[i];
+      people[i] = people[j];
+      people[j] = temp;
+      i--;
+    }
+    return (
+      this.setState({ characters: people })
+    );
+  }
+
+  // Function to set all character's "clicked" back to false
+  clickedToFalse = () => {
 
   }
 
-  // Function to randomize rendered characters and restart the game
-  restartGame = () => {
-
+  // shuffleData = data => {
+  //   let i = data.length - 1;
+  //   while (i > 0) {
+  //   const j = Math.floor(Math.random() * (i + 1));
+  //   const temp = data[i];
+  //   data[i] = data[j];
+  //   data[j] = temp;
+  //   i--;
+  //   }
+  //   return data;
+  // };
+  
+  
+  
+  // Function to restart the game
+  restartGame = () => { 
+    this.getHighScore();
+    this.resetScore();
+    this.randomizeCards();
+    this.clickedToFalse();
   }
 
   // Function to increase score when a new character is clicked, or reset the score to 0 and update the top score if a character is clicked twice
@@ -33,10 +81,10 @@ class App extends React.Component {
     const selected = this.state.characters.filter(characters => characters.id === id);
     if(selected[0].clicked === false) {
       this.increaseScore();
+      this.randomizeCards();
       selected[0].clicked = true;
     } else {
-      this.resetScore();
-      // restartGame();
+      this.restartGame();
     }
   };
 
@@ -51,17 +99,23 @@ class App extends React.Component {
         </nav>
         <div class="jumbotron"></div>
         <div className="text-center title">Freaks and Geeks</div>
-        <div className="card-container"> 
-          {this.state.characters.map((characters, index) => {
-            return <CharacterCard 
-              key={index}
-              id={characters.id}
-              species={characters.species}
-              image={characters.image} 
-              clicked={characters.clicked}
-              handleOnClick={this.clicker}
-            />
-          })}
+        <div className="card-container">
+          <div className="row">
+            {this.state.characters.map((characters, index) => {
+              return (
+                <div id='cardItem' className="col-lg-3">
+                  <CharacterCard 
+                    key={index}
+                    id={characters.id}
+                    name={characters.name}
+                    image={characters.image} 
+                    clicked={characters.clicked}
+                    handleOnClick={this.clicker}
+                   />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </>
     );
