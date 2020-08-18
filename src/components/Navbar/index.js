0dff6ad8b4fed085e2b55246871a66d3ec9fcc8b
Megan Swenson <mrsmeganswenson@gmail.com>
2020-08-18 15:35:41 -0600

    Score divs on navbar to display on right side

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,10 +5,10 @@ function Navbar(props) {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light">
-                <h4 className="nav-header">Clicky Game</h4>
+                <h4 className="nav-header">Freaks & Geeks Clicky Game</h4>
                 <div className="score-box">
-                    <p id="score">Score: {props.score}</p>
-                    <p id="top-score">Top Score: {props.highScore}</p>
+                    <p className="score" id="score">Score: {props.score}</p>
+                    <p className="top-score" id="top-score">Top Score: {props.highScore}</p>
                 </div>
             </nav>
         </>
