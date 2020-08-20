8694e0a779df060789ea8beab3cbae1ec001329c
Megan Swenson <mrsmeganswenson@gmail.com>
2020-08-20 16:42:06 -0600

    Updates

---
+++ precache-manifest.02061ca1d4ab1a98480bce3af75e4ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c79d74927089f6e016d3ba5d2392d7",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "956a7fb1761f05d36185",
+    "url": "/react-clicky-game/static/css/main.1d7aecb8.chunk.css"
+  },
+  {
+    "revision": "b664ec023ea0d0104e95",
+    "url": "/react-clicky-game/static/js/2.ef1e93f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clicky-game/static/js/2.ef1e93f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956a7fb1761f05d36185",
+    "url": "/react-clicky-game/static/js/main.609fdbdd.chunk.js"
+  },
+  {
+    "revision": "27449a2a255f825bc27e",
+    "url": "/react-clicky-game/static/js/runtime-main.6947ff73.js"
+  },
+  {
+    "revision": "fb35a5b76e2d48306d09f328242beb95",
+    "url": "/react-clicky-game/static/media/lockers.fb35a5b7.jpg"
+  }
+]);
